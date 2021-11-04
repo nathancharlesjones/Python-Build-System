ea503bd74b48049b869e427a3d3b9e5d0d46e440
Nathan Jones <55743963+nathancharlesjones@users.noreply.github.com>
2021-11-04 07:45:05 -0400

    Added make.py

---
+++ make.py
@@ -0,0 +1,20 @@
+import json
+import sys
+
+def main():
+    try:
+        recipes_file = open('../recipes.json','r')
+    except IOError:
+        print("**ERROR**: Could not find '../recipes.json'. Ensure that this file is present in the project root.")
+        sys.exit()
+
+    try:
+        recipes = json.load(recipes_file)
+    except json.decoder.JSONDecodeError:
+        print("**ERROR**: Could not read '../recipes.json'. Ensure that this file has proper JSON syntax and follows the project conventions for defining recipe values. See here for more information.")
+        sys.exit()
+
+
+
+if __name__ == "__main__":
+    main()
