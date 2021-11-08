510cea76558524bbe3194f12787b39f7246bf447
Nathan Jones <55743963+nathancharlesjones@users.noreply.github.com>
2021-11-08 17:55:17 -0500

    Updated README

--- example_project_targets.py
+++ example_project_targets.py
@@ -6,37 +6,44 @@ import target
 def get_project_targets():
 	targets = {}
 
-	libtest_build_dir = "build-Simple-Build-System"
+	libtest_name = "libtest"
+	libtest_build_dir = "build/" + libtest_name
+	libtest_target = libtest_name + ".a"
+
 	libtest = target.library(
-		name 				= 	"libtest",
+		name 				= 	libtest_name,
 		c_compiler 			= 	"gcc",
 		archiver 			= 	"ar",
 		archiver_flags 		= 	['rcs'],
 		build_dir 			= 	libtest_build_dir,
-		target 				= 	'libtest.a',
+		target 				= 	libtest_target,
 		source_files 		= 	["lib/test/src/test_func.c"],
 		include_dirs 		= 	["lib/test/inc"],
-		pre_build_cmds 		= 	["echo Beginning build for libtest"],
-		post_build_cmds 	= 	["echo Finished building libtest"]
+		pre_build_cmds 		= 	["echo Beginning build for " + libtest_name],
+		post_build_cmds 	= 	["echo Finished building " + libtest_name]
 	)
 
 	targets[libtest.name] = libtest
 
+	hello_world_name = "hello_world"
+	hello_world_build_dir = "build/" + hello_world_name
+	hello_world_target = hello_world_name + ".exe"
+
 	hello_world = target.executable(
-		name 				= 	'hello_world',
+		name 				= 	hello_world_name,
 		c_compiler 			= 	'gcc',
 		c_flags 			= 	['-g3','-O0'],
 		linker 				= 	'gcc',
-		build_dir 			= 	'build-Simple-Build-System',
-		target 				= 	'main.exe',
+		build_dir 			= 	hello_world_build_dir,
+		target 				= 	hello_world_target,
 		source_files 		= 	['src/main.c'],
 		include_dirs 		= 	["lib/test/inc"],
 		libraries 			= 	["test", "m"],
 		library_dirs 		= 	[libtest_build_dir],
 		local_dependencies 	= 	[libtest],
-		pre_build_cmds 		= 	["echo Beginning build for hello-world"],
-		post_build_cmds 	= 	[	"echo Finished building hello-world",
-									"./build-Simple-Build-System/main.exe"]
+		pre_build_cmds 		= 	["echo Beginning build for " + hello_world_name],
+		post_build_cmds 	= 	["echo Finished building " + hello_world_name,
+								 "./{0}/{1}".format(hello_world_build_dir, hello_world_target)]
 	)
 
 	targets[hello_world.name] = hello_world
